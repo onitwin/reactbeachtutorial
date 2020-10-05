0a76173d513c0b8f0b1f46da8dcaed1e918e04b5
onitwin <onitwin@gmail.com>
2020-10-05 15:13:40 +0100

    basic set up complete for tutorial

---
+++ src/pages/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Error=()=>{
+  return(
+    <div>
+    Hello from Error page
+    </div>
+  )
+}
+
+export default Error;
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class SingleRoom extends Component{
+  render(){
+    return(
+      <div>
+    hello from the Sinle Rooms Page
+    </div>
+    )
+  }
+}
+
+export default SingleRoom ;
