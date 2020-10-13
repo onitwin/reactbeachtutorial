4599637be302816d094121606bfb06967bfb664f
onitwin <onitwin@gmail.com>
2020-10-13 15:30:23 +0100

    minor revisions for legitibility

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react"
 import {RoomContext} from "../Context"
 
+
 export default class FeaturedRooms extends Component{
   static contextType=RoomContext;
   render(){
